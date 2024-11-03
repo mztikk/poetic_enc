da60cbf1ffa9e5ccf3e0160e6361f4bc73673c47
mztikk <mztikk@outlook.de>
2024-11-04 00:50:17 +0100

    use varying separator, added test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use atty::Stream;
-use rand::Rng;
+use rand::{distributions::Standard, prelude::Distribution, Rng};
 use std::{
     collections::HashMap,
     io::{self, Read},
+    str::Chars,
     sync::LazyLock,
 };
 
@@ -24,6 +25,31 @@ fn get_word_of_len(len: usize) -> String {
         Some(words) => words[rand::thread_rng().gen_range(0..words.len())].to_string(),
         None => str::repeat("a", len),
     }
+}
+
+struct Separator {
+    x: &'static str,
+}
+
+impl Distribution<Separator> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Separator {
+        const SEPARATORS: [&str; 31] = [
+            " ", "#", "+", "-", "_", ":", ";", ".", ",", "*", "/", "(", ")", "[", "]", "=", "!",
+            "?", "$", "%", "&", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        ];
+        Separator {
+            x: SEPARATORS[rng.gen_range(0..SEPARATORS.len())],
+        }
+    }
+}
+
+fn get_separator() -> String {
+    rand::thread_rng()
+        .sample_iter(Standard)
+        .take(rand::thread_rng().gen_range(1..5))
+        .map(|f: Separator| f.x)
+        .collect::<Vec<&'static str>>()
+        .join("")
 }
 
 fn get_inc() -> String {
@@ -87,11 +113,21 @@ fn translate_to_inc(n: i32) -> String {
     let mut result = Vec::new();
 
     for _ in 0..times {
-        result.push(format!("{} {}", get_inc(), get_word_of_len(9)));
+        result.push(format!(
+            "{}{}{}",
+            get_inc(),
+            get_separator(),
+            get_word_of_len(9)
+        ));
     }
 
     if rest > 0 {
-        result.push(format!("{} {}", get_inc(), get_word_of_len(rest as usize)));
+        result.push(format!(
+            "{}{}{}",
+            get_inc(),
+            get_separator(),
+            get_word_of_len(rest as usize)
+        ));
     }
 
     result.join("\n")
@@ -104,14 +140,74 @@ fn translate_to_dec(n: i32) -> String {
     let mut result = Vec::new();
 
     for _ in 0..times {
-        result.push(format!("{} {}", get_dec(), get_word_of_len(9)));
+        result.push(format!(
+            "{}{}{}",
+            get_dec(),
+            get_separator(),
+            get_word_of_len(9)
+        ));
     }
 
     if rest > 0 {
-        result.push(format!("{} {}", get_dec(), get_word_of_len(rest as usize)));
+        result.push(format!(
+            "{}{}{}",
+            get_dec(),
+            get_separator(),
+            get_word_of_len(rest as usize)
+        ));
     }
 
     result.join("\n")
+}
+
+struct Encoder<'a> {
+    input_iter: Chars<'a>,
+    last_value: u8,
+    is_end: bool,
+}
+
+impl<'a> Encoder<'a> {
+    pub fn new(input: &'a str) -> Self {
+        let iter: Chars<'_> = input.chars().into_iter();
+        Encoder {
+            input_iter: iter,
+            last_value: 0,
+            is_end: false,
+        }
+    }
+}
+
+impl<'a> Iterator for Encoder<'a> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.is_end {
+            return None;
+        }
+
+        match self.input_iter.next() {
+            Some(c) => {
+                let ascii_value = c as u8;
+                let diff_to_last = ascii_value as i32 - self.last_value as i32;
+                self.last_value = ascii_value;
+
+                Some(format!(
+                    "{}{}{}",
+                    match diff_to_last.cmp(&0) {
+                        std::cmp::Ordering::Less => translate_to_dec(diff_to_last.abs()),
+                        std::cmp::Ordering::Greater => translate_to_inc(diff_to_last.abs()),
+                        std::cmp::Ordering::Equal => String::from(""),
+                    },
+                    get_separator(),
+                    get_out()
+                ))
+            }
+            None => {
+                self.is_end = true;
+                Some(get_end())
+            }
+        }
+    }
 }
 
 fn main() {
@@ -123,27 +219,43 @@ fn main() {
     let mut buf = String::new();
     stdin.read_to_string(&mut buf).unwrap();
 
-    let chars = buf.chars().collect::<Vec<char>>();
+    let encoder = Encoder::new(buf.as_str());
+    encoder.for_each(|x| println!("{}", x));
+}
 
-    let mut last_val: u8 = 0;
-    for c in chars {
-        let ascii_value = c as u8;
-        let diff_to_last = ascii_value as i32 - last_val as i32;
+#[cfg(test)]
+mod tests {
+    use std::sync::{Arc, Mutex};
 
-        match diff_to_last.cmp(&0) {
-            std::cmp::Ordering::Less => {
-                println!("{}", translate_to_dec(diff_to_last.abs()));
-            }
-            std::cmp::Ordering::Greater => {
-                println!("{}", translate_to_inc(diff_to_last.abs()));
-            }
-            std::cmp::Ordering::Equal => {}
-        }
+    use poetic::{interpreter::Interpreter, parser::Parser};
 
-        println!("{}", get_out());
+    use super::*;
 
-        last_val = ascii_value;
+    fn run_and_get_output(input: &str) -> String {
+        let encoded = Encoder::new(input).collect::<Vec<String>>().join(" ");
+        let intermediate = Parser::parse_intermediate(&encoded);
+        let instructions = Parser::parse_instructions(&intermediate).unwrap();
+        let result = Arc::new(Mutex::new(String::new()));
+        let result_clone = result.clone();
+
+        let mut interpreter = Interpreter::new(instructions).with_output(Box::new(move |s| {
+            result_clone.lock().unwrap().push_str(s.as_str());
+        }));
+        interpreter.run();
+
+        return result.lock().unwrap().to_string();
     }
 
-    println!("{}", get_end());
+    #[test]
+    fn encoded_should_decode() {
+        let input = "test";
+        let result = run_and_get_output(input);
+
+        assert_eq!(result, input);
+
+        let input = "täßt";
+        let result = run_and_get_output(input);
+
+        assert_eq!(result, input);
+    }
 }
